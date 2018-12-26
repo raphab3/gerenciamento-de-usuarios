56d8a7290163504ad9a51b4c14fb5256007b4950
Rafael <raphab33@hotmail.com>
2018-12-26 12:47:06 -0300

    adicionando dados temporarios dos usuarios na tabela

--- index.js
+++ index.js
@@ -9,29 +9,63 @@
 
 
 
-// -------------Usando forEch -
+// -------------Menu
 
+var menu = document.querySelector("#body").addEventListener("click", function(event){
+    // menu.classList.value = "hold-transition skin-blue sidebar-mini sidebar-collapse";
+    var classe = event.path[4].className;
+    
+    if(classe == "hold-transition skin-blue sidebar-mini"){
+        document.querySelector("body").classList = "hold-transition skin-blue sidebar-mini sidebar-collapse";
+    }else{
+        document.querySelector("body").classList = "hold-transition skin-blue sidebar-mini";
+    }
+});
+
+// Pegando dados do formUser
 
 var fields = document.querySelectorAll("#form-user-create [name]");
-
 var user = {};
 
-document.getElementById("form-user-create").addEventListener("submit", function(event){
-        event.preventDefault();
+function addLine(dataUser){
+    console.log("Dados do usuario", dataUser);
+    
+    var tr = document.createElement('tr');
+    
+    tr.innerHTML = `
+    <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${user.name}</td>
+        <td>${user.email}</td>
+        <td>${user.admin}</td>
+        <td>${user.birth}</td>
+        <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </tr>
+    
+    `;
+    document.getElementById('tableUsers').appendChild(tr);
+    
+}
 
-        fields.forEach((dados) => {
-            if (dados.name == "gender") {
-                
-                if (dados.checked){
-                    user[dados.name] = dados.value;
-                }
-        
-            }
-            else{
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+    event.preventDefault();
+    
+    fields.forEach((dados) => {
+        if (dados.name == "gender") {
+            
+            if (dados.checked){
                 user[dados.name] = dados.value;
             }
+            
+        }
+        else{
+            user[dados.name] = dados.value;
+        }
         
-        });
-
-        console.log(user);
+    });
+    
+    addLine(user);
 });
